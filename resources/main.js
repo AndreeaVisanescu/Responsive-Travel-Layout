b717b21ac71e9db75603ae43647c94663617db64
Andreea Visanescu <74706560+AndreeaVisanescu@users.noreply.github.com>
2021-02-17 14:56:47 +0200

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -7,15 +7,13 @@ let bodyWrapper = document.getElementById('bodyWrapper');
 let mobileMenu = document.getElementById('mobileMenu');
 
 seeMenu.addEventListener('click', function(){
-    if (mobileMenu.classList.contains('hidden')) {
-        mobileMenu.classList.remove('hidden');
-        bodyWrapper.classList.add('hidden');
-    } 
+    (mobileMenu.classList.contains('hidden'))
+     mobileMenu.classList.remove('hidden');
+     bodyWrapper.classList.add('hidden');
 });
 
 removeMenu.addEventListener('click', function(){
-        if (!mobileMenu.classList.contains('hidden')) {
-            mobileMenu.classList.add('hidden');
-            bodyWrapper.classList.remove('hidden');
-        }
+        !mobileMenu.classList.contains('hidden')) 
+         mobileMenu.classList.add('hidden');
+         bodyWrapper.classList.remove('hidden');
 });
