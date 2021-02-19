6abf65a3bd3151d913085ee10912198399b774d3
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2021-02-19 13:13:06 +0200

    code review

--- resources/main.js
+++ resources/main.js
@@ -7,13 +7,17 @@ let bodyWrapper = document.getElementById('bodyWrapper');
 let mobileMenu = document.getElementById('mobileMenu');
 
 seeMenu.addEventListener('click', function(){
+    // ce face acest set de paranteze?
+    // daca ai vrut ternary, nu asta este sintaxa
+    // just fyi nu ai nevoie de if aici, putem sa dam click ori pe menu item ori pe x, la fel si mai jos
     (mobileMenu.classList.contains('hidden'))
      mobileMenu.classList.remove('hidden');
      bodyWrapper.classList.add('hidden');
 });
 
 removeMenu.addEventListener('click', function(){
-        !mobileMenu.classList.contains('hidden')) 
+    // din nou, nu inteleg foarte bine ce se intampla aici
+        !mobileMenu.classList.contains('hidden')
          mobileMenu.classList.add('hidden');
          bodyWrapper.classList.remove('hidden');
 });
